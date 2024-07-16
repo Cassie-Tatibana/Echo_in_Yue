29c9bf0819d6ffbb3bde78cf9126999d320b6088
tatibana <xinmiaotang02@gmail.com>
2024-07-16 15:05:12 +0800

    船

--- index_scripts.js
+++ index_scripts.js
@@ -1,26 +1,36 @@
 function setMode(mode) {
     const body = document.body;
-    const contentDiv = document.getElementById('content');
-    contentDiv.innerHTML = '';
-
+    const titleDiv = document.getElementById('title');
+    const SpecAnimaDiv = document.getElementById('SpecAnima');
+    titleDiv.innerHTML = '';
+    SpecAnimaDiv.innerHTML = '';
     if (mode === 'day') {
         body.style.backgroundImage = "url('image/白天/白天_背景.png')";
         
-        const img = document.createElement('img');
-        img.src = 'image/白天/标题（白天）.png'; 
-        img.alt = 'Day Mode Image';
-        contentDiv.appendChild(img);
+        const img1 = document.createElement('img');
+        const img2 = document.createElement('img');
+        img1.src = 'image/白天/标题（白天）.png'; 
+        img1.alt = 'Day Mode Image';
+        img2.src = 'image/白天/船（白天）.png'; 
+        img2.alt = 'Boat';
+        titleDiv.appendChild(img1);
+        SpecAnimaDiv.appendChild(img2);
         
     } else {
         body.style.backgroundImage = "url('image/夜晚/夜晚_桥底光.png')";
       
-        const img = document.createElement('img');
-        img.src = 'image/夜晚/标题（夜晚）.png'; 
-        img.alt = 'Night Mode Image';
-        contentDiv.appendChild(img);
+        const img1 = document.createElement('img');
+        const img2 = document.createElement('img');
+        img1.src = 'image/夜晚/标题（夜晚）.png'; 
+        img1.alt = 'Night Mode Image';
+        img2.src = 'image/夜晚/船（夜晚）.png'; 
+        img2.alt = 'Boat';
+        titleDiv.appendChild(img1);
+        SpecAnimaDiv.appendChild(img2);
     }
 
     localStorage.setItem('preferredMode', mode);
+    
 }
 
 function checkTime() {
