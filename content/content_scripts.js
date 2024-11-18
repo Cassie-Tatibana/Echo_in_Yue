ee25a91a490b254a2d72ad3979e3cf48a8c9c2fc
tatibana <xinmiaotang02@gmail.com>
2024-11-18 23:18:57 +0800

    内容页声音

--- content/content_scripts.js
+++ content/content_scripts.js
@@ -18,3 +18,23 @@ function toChapter4(){
 
 
 
+
+// 获取音频元素和图标元素
+var soundIcon = document.getElementById('soundIcon');
+var music = document.getElementById('backgroundMusic');
+
+// 初始化时，确保图标不旋转
+soundIcon.classList.remove('rotate-icon');
+
+// 监听图标的点击事件
+soundIcon.addEventListener('click', function() {
+    if (music.paused) {
+        // 如果音频已暂停，则播放音频并旋转图标
+        music.play();
+        soundIcon.classList.add('rotate-icon');
+    } else {
+        // 如果音频正在播放，则暂停音频并停止图标旋转
+        music.pause();
+        soundIcon.classList.remove('rotate-icon');
+    }
+});
