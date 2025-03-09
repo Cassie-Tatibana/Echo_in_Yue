7038dacfd6737fb86844b9efc14cdaf5a561cb41
tatibana <xinmiaotang02@gmail.com>
2025-03-09 17:18:48 +0800

    测试好了,已修改内容跳转至目录页的白屏

--- content/content_scripts.js
+++ content/content_scripts.js
@@ -1,18 +1,18 @@
 
 function toChapter1(){
-    window.location.href="../../Chapter/Chapter_one/Chapter_one.html";
+    window.location.href="../../chapter/chapter_one/chapter_one.html";
 }
 
 function toChapter2(){
-    window.location.href="../../Chapter/Chapter_two/Chapter_two.html";
+    window.location.href="../../chapter/chapter_two/chapter_two.html";
 }
 
 function toChapter3(){
-    window.location.href="../../Chapter/Chapter_three/Chapter_three.html";
+    window.location.href="../../chapter/chapter_three/chapter_three.html";
 }
 
 function toChapter4(){
-    window.location.href="../../Chapter/Chapter_four/Chapter_four.html";
+    window.location.href="../../chapter/chapter_four/chapter_four.html";
 }
 
 
